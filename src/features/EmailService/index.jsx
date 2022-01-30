8c09b3dfe832838d78b814d56d2eeb23f0128b15
Benjamin Leray <b.leray35@gmail.com>
2022-01-30 16:39:09 +0100

    add first layout setup

---
+++ src/features/EmailService/index.jsx
@@ -0,0 +1,15 @@
+
+const emailServiceStyle = {
+  backgroundColor: '#e8846d',
+  width: '100%',
+};
+
+const EmailService = () => {
+  return (
+    <div className='EmailService' style={emailServiceStyle}>
+      <h3>Email Service</h3>
+    </div>
+  );
+}
+
+export default EmailService;
---
+++ src/features/PageService/index.jsx
@@ -0,0 +1,16 @@
+
+const emailServiceStyle = {
+  backgroundColor: '#716de8',
+  width: '100%',
+};
+
+
+const PageService = () => {
+  return (
+    <div className='pageService' style={emailServiceStyle}>
+      <h3>Page Service</h3>
+    </div>
+  );
+}
+
+export default PageService;
